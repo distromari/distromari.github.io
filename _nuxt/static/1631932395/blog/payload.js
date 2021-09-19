fb8c3f6a9bb65def2555b6a667fc2c6a2824b5d4
distromari <distromari@users.noreply.github.com>
2021-09-19 03:51:32 +0000

    deploy: a51a0eaa472f5ec2417d0f7137acc400307c53b4

--- _nuxt/static/1631932395/blog/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/blog", {data:[{articles:[{slug:"post1",description:"hey.",title:"Index"}]}],fetch:{},mutations:[]});
---
+++ _nuxt/static/1632023491/illustrations/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/illustrations", {data:[{}],fetch:{},mutations:void 0});
