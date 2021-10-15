1e63ea26fd25d2b95801c7a6c637b275f266b350
distromari <distromari@users.noreply.github.com>
2021-10-15 21:18:50 +0000

    deploy: 8fc74ef56c495a8e85d85870dc55acc12dabc382

---
+++ _nuxt/static/1634332729/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fillustrations","\u002Fprojects","\u002Freadme","\u002Fblog","\u002Fblog\u002Fpost1"]})
---
+++ _nuxt/static/1634332729/projects/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634332729",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c46e9282"}}}}(null));
