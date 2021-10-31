a9233b5fb73849cd88c317c6366e1f8af5b0dac2
distromari <distromari@users.noreply.github.com>
2021-10-31 18:02:25 +0000

    deploy: d9875f15a739bfd62bd5d41b09c59b8b800d7a7f

--- _nuxt/static/1635698766/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635698766",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a0b088df"}}}}(null,"\u002F"));
